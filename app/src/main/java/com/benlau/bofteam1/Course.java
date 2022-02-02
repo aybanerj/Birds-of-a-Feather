050926a2a7772caf15a40e4be0942b8339616a7b
aybanerj <ayushi.banerjee@Amplussolar.com>
2022-02-01 22:24:23 -0800

    course class

---
+++ app/src/main/java/com/benlau/bofteam1/Course.java
@@ -0,0 +1,21 @@
+package com.benlau.bofteam1;
+
+public class Course {
+    public String year;
+    public String quarter;
+    public String courseName;
+    public String courseNumber;
+
+    Course(String year, String quarter, String courseName, String courseNumber){
+        this.year = year;
+        this.quarter = quarter;
+        this.courseName = quarter;
+        this.courseNumber = courseNumber;
+    }
+
+    public String courseConcat(){
+        String combined = this.year+this.quarter+this.courseName+this.courseNumber;
+        return combined;
+    }
+
+}
