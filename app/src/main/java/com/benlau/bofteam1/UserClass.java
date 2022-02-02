16370964699b2a434312bc56b07c91488dcf41c8
waterdud <wujustin1@gmail.com>
2022-02-01 22:19:00 -0800

    story 2 UI

--- app/src/main/java/com/benlau/bofteam1/UserClass.java
+++ app/src/main/java/com/benlau/bofteam1/UserClass.java
@@ -3,6 +3,13 @@ package com.benlau.bofteam1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Toast;
 
 public class UserClass extends AppCompatActivity {
 
@@ -10,5 +17,18 @@ public class UserClass extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_class);
+
+
+        Spinner quarter = findViewById(R.id.quarter);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item,
+                new String[]{"FA", "WI", "SPR", "S1", "S2"});
+        quarter.setPrompt("QUARTER");
+        quarter.setAdapter(adapter);
+
+
+    }
+
+    public void goBack(View view) {
+        finish();
     }
 }
