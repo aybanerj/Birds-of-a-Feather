5e190db15e290fecf13da8488a4820815ca3ee49
Yizhang Liu <benjilewiscn@gmail.com>
2022-02-01 19:38:23 -0800

    FirstName class added.

---
+++ app/src/main/java/com/benlau/bofteam1/FirstName.java
@@ -0,0 +1,17 @@
+package com.benlau.bofteam1;
+
+public class FirstName {
+  private String firstName;
+
+  public FirstName(String name) {
+    firstName = name;
+  }
+
+  public void setName(String name) {
+    firstName = name;
+  }
+
+  public String getName() {
+    return firstName;
+  }
+}
