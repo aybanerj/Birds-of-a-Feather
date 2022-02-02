5fb0f46ed49ba8e7882dd4d41be4c7f3d9571180
waterdud <wujustin1@gmail.com>
2022-02-01 20:28:45 -0800

    test push

---
+++ app/src/main/java/com/benlau/bofteam1/UserClass.java
@@ -0,0 +1,14 @@
+package com.benlau.bofteam1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserClass extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_class);
+    }
+}
