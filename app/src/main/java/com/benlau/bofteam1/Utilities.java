6a71a6ead6737c1891514b57d4a8eeab8c5a201a
waterdud <wujustin1@gmail.com>
2022-02-06 20:41:14 -0800

    fixed bugs

---
+++ app/src/main/java/com/benlau/bofteam1/Utilities.java
@@ -0,0 +1,23 @@
+package com.benlau.bofteam1;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+
+import java.util.Optional;
+
+public class Utilities {
+
+    public static void showAlert(Activity activity, String message) {
+        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(activity);
+
+        alertBuilder
+                .setTitle("Alert")
+                .setMessage(message)
+                .setPositiveButton("Ok", (dialog, id) -> {
+                    dialog.cancel();
+                })
+                .setCancelable(true);
+        AlertDialog alertDialog = alertBuilder.create();
+        alertDialog.show();
+    }
+}
