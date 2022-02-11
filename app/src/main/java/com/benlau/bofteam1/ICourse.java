e326e889f767697b9dd796c77303861003766044
Victoria <vsasaoka@gmail.com>
2022-02-11 13:59:31 -0800

    Set up outline for PersonsViewAdapter

--- app/src/main/java/com/benlau/bofteam1/ICourse.java
+++ app/src/main/java/com/benlau/bofteam1/ICourse.java
@@ -1,13 +1,12 @@
 package com.benlau.bofteam1;
 
-import java.util.List;
-
 public interface ICourse {
     int getId();
     String getFullCourse();
     String getQuarter();
     String getCourseName();
     String getCourseNumber();
+
 
 
 }
--- app/src/main/java/com/benlau/bofteam1/PersonsViewAdaper.java
+++
@@ -1,6 +0,0 @@
-package com.benlau.bofteam1;
-
-public class PersonsViewAdaper {
-    public static class ViewHolder {
-    }
-}
